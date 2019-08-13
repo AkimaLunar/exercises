41f0671f478d405b92dae44b6501b91774ba0bd4
Ria Nicole Carmin <akimalunar@gmail.com>
2019-08-12 17:22:13 -0700

    Card 2.0

--- deck_of_playing_cards/Card.js
+++ deck_of_playing_cards/Card.js
@@ -1,37 +1,36 @@
-const constructDisplayValue = value => {
-    switch (value) {
-    case 1: {
-        return `Ace`;
-    }
-    case 13: {
-        return `King`;
-    }
-    case 12: {
-        return `Queen`;
-    }
-    case 11: {
-        return `Jack`;
-    }
-    default: {
-        return value;
-    }
-    }
-};
-
 class Card {
     constructor(value, suit) {
-        this.value = value;
         this.suit = suit;
+        this.value = value;
     }
-    /**
-     * toString method returns a display name of the card.
-     * @returns {string}
-     */
+
+    get displayValue() {
+        switch (this.value) {
+            case 1: {
+                return `Ace`;
+            }
+            case 13: {
+                return `King`;
+            }
+            case 12: {
+                return `Queen`;
+            }
+            case 11: {
+                return `Jack`;
+            }
+            default: {
+                return this.value;
+            }
+        }
+    }
+
+    get displaySuit() {
+        const [firstLetter, ...rest] = this.suit;
+        return firstLetter.toUpperCase() + rest.join(``);
+    }
+
     toString() {
-        const [first, ...rest] = this.suit;
-        const displaySuit = `${first.toUpperCase()}${rest.join(``)}`;
-        const displayValue = constructDisplayValue(this.value);
-        return `Card { ${displayValue} of ${displaySuit} }`;
+        return `Card { ${this.displayValue} of ${this.displaySuit} }`;
     }
 }
 
